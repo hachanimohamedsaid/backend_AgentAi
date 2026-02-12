8e36a223f7e66ed784f52460bb419f1c2d8d3776
Aziz111501 <azizouergli908@gmail.com>
2026-02-12 12:35:22 +0100

    Revert "Merge pull request #1 from hachanimohamedsaid/feature-backend/azizZambo"
    This reverts commit 8af05031bf10e95a105875ff2a63ca40653026b3, reversing
    changes made to 6a35fe1573441baf8704112b95f847cf8eaad732.

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,8 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { getConnectionToken } from '@nestjs/mongoose';
-import { Connection } from 'mongoose';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -10,14 +8,7 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [
-        AppService,
-        // Provide a mock mongoose connection for unit tests
-        {
-          provide: getConnectionToken(),
-          useValue: ({ readyState: 1 } as unknown) as Connection,
-        },
-      ],
+      providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
