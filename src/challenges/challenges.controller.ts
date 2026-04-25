6f0ac61b78d6fed595ff00fce09922566c3f30d5
Hachani-mohamedsaid <mohamedsaid.Hachani@esprit.tn>
2026-04-25 16:36:05 +0100

    test

--- src/challenges/challenges.controller.ts
+++ src/challenges/challenges.controller.ts
@@ -1,9 +1,7 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { KeycloakAuthGuard } from '../auth/keycloak-auth.guard';
 import { ChallengesService } from './challenges.service';
 
 @Controller(['challenges', 'api/challenges'])
-@UseGuards(KeycloakAuthGuard)
 export class ChallengesController {
   constructor(private readonly challengesService: ChallengesService) {}
 
